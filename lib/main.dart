a7047e2f4fb4b1497b9b21f3bb82def39b8e737f
Yoangeliwon <yemisrachhabtamu4@gmail.com>
2026-05-01 01:23:35 +0300

    Modified

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,40 @@ void main() {
   runApp(const HabitApp());
 }
 
-class HabitApp extends StatelessWidget {
+class HabitApp extends StatefulWidget {
   const HabitApp({super.key});
+
+  @override
+  State<HabitApp> createState() => _HabitAppState();
+}
+
+class _HabitAppState extends State<HabitApp> {
+  // Global state for Dark Mode
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void toggleTheme(bool isDark) {
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'HabitFlow Tracker',
+      title: 'HabitFlow',
       theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
         brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      // Topic 3: Initial Navigation to the Main Navigation shell
-      home: const MainNavigation(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      themeMode: _themeMode, // This is what makes it work!
+      home: MainNavigation(onThemeChanged: toggleTheme),
     );
   }
 }
