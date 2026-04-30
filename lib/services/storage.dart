d1161b0afb32ecdea47bd9236119046ef4943f47
abdimuqtadirmohamed-tech <abdimuqtadirmohamed@gmail.com>
2026-04-30 15:50:43 +0300

    Update storage.dart

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -5,26 +5,15 @@ class StorageService {
   static const String _key = "habits";
 
   static Future<void> saveHabits(List<Map<String, dynamic>> habits) async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      final encodedData = jsonEncode(habits);
-      await prefs.setString(_key, encodedData);
-    } catch (e) {
-      // ignore error silently for now
-    }
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, jsonEncode(habits));
   }
 
   static Future<List<Map<String, dynamic>>> loadHabits() async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      final data = prefs.getString(_key);
-
-      if (data == null) return [];
-
-      final List decoded = jsonDecode(data);
-      return decoded.map((e) => Map<String, dynamic>.from(e)).toList();
-    } catch (e) {
-      return [];
-    }
+    final prefs = await SharedPreferences.getInstance();
+    final data = prefs.getString(_key);
+    if (data == null) return [];
+    final List decoded = jsonDecode(data);
+    return decoded.map((e) => Map<String, dynamic>.from(e)).toList();
   }
 }
